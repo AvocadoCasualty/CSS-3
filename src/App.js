feb540db2bcc31d9c13bcd5b4644d33b6f61ea69
AvocadoCasualty <baibeebambi@gmail.com>
2020-05-25 10:56:21 -0700

    Woo it's working!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Nav from "./Nav";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            toggleMenu: false
+        }
+    }
+openCloseMenu (){
+        this.setState({
+            toggleMenu: !this.state.toggleMenu
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <div className="logo">
+                        <img alt="Logo"
+                             src="https://blackrockdigital.github.io/startbootstrap-agency/assets/img/navbar-logo.svg"/>
+                    </div>
+                    <div className="nav-desktop">
+                        <ul>
+                            <a className="nav-link" href="#">SERVICES </a>
+                            <a className="nav-link" href="#">PORTFOLIO </a>
+                            <a className="nav-link" href="#">ABOUT </a>
+                            <a className="nav-link" href="#">TEAM </a>
+                            <a className="nav-link" href="#">CONTACT </a>
+                        </ul>
+                    </div>
+                    <div className={this.state.toggleMenu ? "nav-mobile open" : "nav-mobile closed"}>
+                        <button className='menu-button' onClick={(e) => this.openCloseMenu()}>MENU <i className="fa fa-bars" aria-hidden="true"></i></button>
+                        <div id="mobileMenu">
+                            <ul>
+                                <a className="nav-link" href="#">SERVICES </a>
+                                <a className="nav-link" href="#">PORTFOLIO </a>
+                                <a className="nav-link" href="#">ABOUT </a>
+                                <a className="nav-link" href="#">TEAM </a>
+                                <a className="nav-link" href="#">CONTACT </a>
+                            </ul>
+                        </div>
+                    </div>
+                </header>
+                <div className="hero-text">
+                    <h1><span>Welcome To Our Studio!</span><br/> IT'S NICE TO MEET YOU</h1>
+                    <button className="tell-me-more">TELL ME MORE</button>
+                </div>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+function Nav () {
+
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+export default Nav
